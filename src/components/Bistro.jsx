7a53ed4348b19e59d73d57909843831608eaf0da
Majidul Islam <160348093+pro-majidul@users.noreply.github.com>
2024-12-29 02:26:32 +0600

    bistro boss section added

---
+++ src/components/Bistro.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import bgImage1 from '../assets/home/chef-service.jpg'
+
+const Bistro = () => {
+  return (
+    <div>
+      <div
+        className={`hero min-h-[calc(80vh-320px)]`}
+        style={{
+          backgroundImage: `url(${bgImage1})`,
+          opacity: 1,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center'
+        }}
+      >
+        <div className='hero-content bg-white w-10/12 mx-auto py-6     text-neutral-content text-center'>
+          <div className='max-w-4xl py-5'>
+            <h1 className='mb-5 md:text-5xl text-2xl font-normal font-Cinzel text-black'>
+              Bistro Boss
+            </h1>
+            <p className='mb-5'>
+              Competently conceptualize optimal methods of empowerment without
+              cross functional sources. Quickly synthesize worldwide web
+              services rather than parallel core competencies. Objectively
+              evisculate enterprise-wide technologies rather than seamless
+              testing procedures. 
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Bistro
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Banner from '../components/banner/Banner'
 import Category from '../components/category/Category'
+import Bistro from '../components/Bistro'
 
 const Home = () => {
   return (
@@ -13,12 +14,23 @@ const Home = () => {
           --From 11:00am to 10:00pm--
         </p>
         <h3 className='text-center  text-3xl uppercase my-8 text-orange-400 font-light underline underline-offset-16 decoration-gray-700  decoration-wavy'>
-          Order online{' '}
+          Order online
         </h3>
 
         <div className='py-5 my-5'>
           <Category></Category>
         </div>
+      </section>
+      <section className='mb-5'>
+        <Bistro></Bistro>
+      </section>
+      <section className='md:my-10 md:py-10 py-5 my-5 '>
+        <p className='text-yellow-300 italic text-center underline underline-offset-16 decoration-gray-700  decoration-wavy'>
+          --Check it Out--
+        </p>
+        <h3 className='text-center  text-3xl uppercase my-8 text-orange-400 font-light underline underline-offset-16 decoration-gray-700  decoration-wavy'>
+         From Our Menu
+        </h3>
       </section>
     </div>
   )
