f6b50909556ac474ec8c088200e3e028a593c855
Majidul Islam <160348093+pro-majidul@users.noreply.github.com>
2024-12-29 20:02:25 +0600

    footer area added

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,16 +1,23 @@
 import {createBrowserRouter} from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 import Home from "../pages/Home";
+import Menu from "../pages/Menu";
+import Error from "../pages/Error";
 
 
  export const router = createBrowserRouter([
     {
       path: "/",
       element: <MainLayout></MainLayout>,
+      errorElement : <Error></Error>,
       children : [
         {
             path : '/',
             element : <Home></Home>
+        },
+        {
+          path : "/ourmenu",
+          element : <Menu></Menu>
         }
       ]
     },
