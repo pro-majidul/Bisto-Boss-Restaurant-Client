1ce4b2e156e1effba640e55768263e65c7633774
Majidul Islam <160348093+pro-majidul@users.noreply.github.com>
2025-01-22 04:29:05 +0600

    user reservation added

--- src/pages/Dashboard/UserReservation.jsx
+++ src/pages/Dashboard/UserReservation.jsx
@@ -5,17 +5,21 @@ import { Input } from "@/components/ui/input"
 import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css';
 import { Button } from "@/components/ui/button"
-
-
-
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import TimePicker from 'react-time-picker';
+import 'react-time-picker/dist/TimePicker.css';
+import 'react-clock/dist/Clock.css';
 
 const UserReservation = () => {
+    const [startdate, setStartDate] = useState(new Date());
     const [formData, setFormData] = useState({
         dropdownValue: null,
         name: '',
         email: '',
-        phone: ''
-
+        phone: '',
+        startdate,
+        time: '10:00'
     })
     const handelsubmit = e => {
         e.preventDefault()
@@ -35,14 +39,32 @@ const UserReservation = () => {
     return (
         <div>
             <Section heading='Book A Table' subHeading='Reservation'></Section>
-            <div className='py-5 my-5'>
+            <div className='py-5 my-5 px-4'>
                 <form onSubmit={handelsubmit} className='space-y-4' action="">
-                    <div className='grid grid-cols-2 md:grid-cols-3 gap-4'>
+                    <div className='grid grid-cols-1 md:grid-cols-3 gap-4'>
                         <div>
-                            <Dropdown onChange={(option) => setFormData((prev) => ({ ...prev, dropdownValue: option.value }))} options={options} value={defaultOption} placeholder="Select an option" />
+                            <label className='block' htmlFor="">Date*</label>
+                            <DatePicker
+                                className="w-full  border border-gray-300 p-2 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+                                selected={startdate} onChange={(date) => setStartDate(date)} />
+                        </div>
+                        <div>
+                            <label>Time*</label>
+                            <TimePicker format="hh:mm:ss a"
+                                className="w-full border border-gray-300 p-3 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+                                onChange={(time) => setFormData((prev) => ({ ...prev, time: time }))}
+                                value={formData.time} />
+                        </div>
+                        <div>
+                            <label htmlFor="">Guest*</label>
+                            <Dropdown
+                                onChange={(option) => setFormData((prev) => ({ ...prev, dropdownValue: option.value }))} options={options}
+                                value={defaultOption}
+                                placeholder="Select an option"
+                            />
                         </div>
                     </div>
-                    <div className='grid grid-cols-2 md:grid-cols-3 gap-4'>
+                    <div className='grid grid-cols-1 md:grid-cols-3 gap-4'>
                         <div>
                             <label htmlFor="">Name*</label>
                             <Input onChange={handelOnchange} type="name" name='name' value={formData.name} placeholder="Name" />
@@ -64,7 +86,9 @@ const UserReservation = () => {
 
             <Section heading='Our Location' subHeading='Visite Us'></Section>
             <br />
-            <LocationCard></LocationCard>
+           <div className='px-4'>
+           <LocationCard></LocationCard>
+           </div>
 
 
 
